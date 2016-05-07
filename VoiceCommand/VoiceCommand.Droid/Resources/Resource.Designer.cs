216613d6254960cafadc87fe3542b993531a41b7
sasquad92 <krzysztof.sommerrey@gmail.com>
2016-05-07 17:59:50 +0200

    adding some graphics

--- VoiceCommand/VoiceCommand.Droid/Resources/Resource.Designer.cs
+++ VoiceCommand/VoiceCommand.Droid/Resources/Resource.Designer.cs
@@ -47,6 +47,15 @@ namespace VoiceCommand.Droid
 			// aapt resource value: 0x7f020000
 			public const int icon = 2130837504;
 			
+			// aapt resource value: 0x7f020001
+			public const int logo = 2130837505;
+			
+			// aapt resource value: 0x7f020002
+			public const int Splash_screen = 2130837506;
+			
+			// aapt resource value: 0x7f020003
+			public const int welcome_to_voice_command = 2130837507;
+			
 			static Drawable()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
